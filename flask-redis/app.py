496e6c49f800412fd19a217c20efeb37613c37e5
vbdockers <valentinbirdeanu@yahoo.com>
2026-03-19 15:49:55 +0200

    Updated Flask message

--- flask-redis/app.py
+++ flask-redis/app.py
@@ -7,8 +7,23 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
-    return "This webpage has been viewed "+counter+" time(s)"
+    counter = str(redis.get('hits'), 'utf-8')
+    
+    donkey_art = """
+<pre>
+     |\    /|
+     | \__/ |
+     |      |
+     |      |  ___
+     |   __/  |   \\
+     |  |      \\   \\
+     |  |       |___|
+</pre>
+"""
+    return "I am playing with Docker compose.<br>" + \
+           "This webpage has been viewed " + counter + " time(s)<br>" + \
+           donkey_art
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
